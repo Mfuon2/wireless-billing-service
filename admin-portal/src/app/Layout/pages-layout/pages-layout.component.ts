e2809643879c73608be3fba3c50842cdf36f4a63
Mfuon Leonard <leonard.oduor@apollo.co.ke>
2021-05-05 16:25:52 +0300

    fix side bar

--- admin-portal/src/app/Layout/pages-layout/pages-layout.component.ts
+++ admin-portal/src/app/Layout/pages-layout/pages-layout.component.ts
@@ -2,38 +2,39 @@ import {Component, OnInit} from '@angular/core';
 import {animate, query, style, transition, trigger} from '@angular/animations';
 
 @Component({
-  selector: 'app-pages-layout',
-  templateUrl: './pages-layout.component.html',
-  animations: [
+    selector: 'app-pages-layout',
+    templateUrl: './pages-layout.component.html',
+    animations: [
 
-    trigger('architectUIAnimation', [
-      transition('* <=> *', [
-        query(':enter, :leave', [
-          style({
-            opacity: 0,
-            display: 'flex',
-            flex: '1',
-            flexDirection: 'column'
+        trigger('architectUIAnimation', [
+            transition('* <=> *', [
+                query(':enter', []),
+                query(':enter, :leave', [
+                    style({
+                        opacity: 0,
+                        display: 'flex',
+                        flex: '1',
+                        flexDirection: 'column'
 
-          }),
-        ]),
-        query(':enter', [
-          animate('200ms ease', style({opacity: 1})),
-        ]),
+                    }),
+                ]),
+                query(':enter', [
+                    animate('200ms ease', style({opacity: 1})),
+                ]),
 
-        query(':leave', [
-          animate('200ms ease', style({opacity: 0})),
-        ], {optional: true})
-      ]),
-    ])
-  ]
+                query(':leave', [
+                    animate('200ms ease', style({opacity: 0})),
+                ], {optional: true})
+            ]),
+        ])
+    ]
 })
 export class PagesLayoutComponent implements OnInit {
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
